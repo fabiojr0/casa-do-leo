e0b5462f3086ee578d7a3cdadaa04856f63e44bd
Rafael Ruddy <rafaelruddy@gmail.com>
2023-11-02 23:46:34 -0300

    added API requisitions

--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -1,9 +1,44 @@
+import React, {useState, useRef, useContext} from 'react'
 import Button from "../components/Button";
+import axios from '../api/axios';
+const LOGIN_URL = '/admins/login/'
+import { useNavigate } from 'react-router-dom';
 
 function Admin() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const navigate = useNavigate();
+    const handleSubmit = async e => {
+        e.preventDefault();
+        try{
+            
+            const response = await axios.post(LOGIN_URL, JSON.stringify({email,password}), 
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                withCredentials: true
+            })
+            
+            console.log(JSON.stringify(response?.data))
+            navigate('/admin/doador');
+        }catch(err) {
+            if(!err?.response) {
+                console.log('No server Response')
+            } else if (err.response?.status === 400) {
+                console.log('Misssing username or password')
+            } else if(err.response?.status === 401) {
+                console.log('Unauthorized')
+            } else {
+                console.log('Login failed')
+            }
+        }
+
+    }
+
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="h-[70vh] w-[90vw] max-w-[400px] flex flex-col items-center justify-center gap-12 bg-white shadow-lg border-4 border-verde rounded-xl">
+      <form className="h-[70vh] w-[90vw] max-w-[400px] flex flex-col items-center justify-center gap-12 bg-white shadow-lg border-4 border-verde rounded-xl" onSubmit={handleSubmit}>
         <h1 className="text-2xl font-semibold text-verde">Casa do Léo</h1>
         <h2 className="text-xl font-semibold">Login de Administrador</h2>
         <div className="inputGroup">
@@ -13,6 +48,8 @@ function Admin() {
             id="Usuário"
             autoComplete="off"
             placeholder="marco_antonio"
+            onChange={e => setEmail(e.target.value)}
+            className='max-w-[300px]'
           />
           <label htmlFor="Usuário">Usuário</label>
         </div>
@@ -23,11 +60,20 @@ function Admin() {
             id="Senha"
             autoComplete="off"
             placeholder="********"
+            onChange={e => setPassword(e.target.value)}
+            className='max-w-[300px]'
+
           />
           <label htmlFor="Senha">Senha</label>
         </div>
-        <Button>Entrar</Button>
-      </div>
+        <button type="submit"
+        className={
+          "bg-verde px-4 py-2 text-white rounded-md text-sm hover:bg-green-700 transition-all flex items-center sm:text-lg active:translate-y-1"
+        }
+      >
+        Entrar
+      </button>
+      </form>
     </div>
   );
 }
--- src/routes/AdminDoador.jsx
+++ src/routes/AdminDoador.jsx
@@ -1,63 +1,89 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AdminInputs from "../components/admin/AdminInputs";
 import AdminList from "../components/admin/AdminList";
 import NavbarAdmin from "../components/admin/NavbarAdmin";
 
+import axios from '../api/axios';
+
 function AdminDoador() {
   const [doador, setDoador] = useState(-1);
+  const [doadores, setDoadores] = useState([]);
+  const [newDoador, setNewDoador] = useState([])
+
 
   const inputs = [
     {
       type: "text",
-      label: "CPF",
+      label: "cpf",
       placeholder: "123.123.123-01",
     },
     {
       type: "text",
-      label: "Nome",
+      label: "nome",
       placeholder: "Marco Antônio",
     },
     {
       type: "text",
-      label: "Email",
+      label: "email",
       placeholder: "usuario@email.com",
     },
     {
       type: "text",
-      label: "Endereço",
+      label: "endereco",
       placeholder: "Rua Gastão Gonçalves",
     },
     {
       type: "text",
-      label: "Telefone",
+      label: "telefone",
       placeholder: "21 912341234",
     },
   ];
 
-  
+  useEffect(() => {
+    async function getContacts() {
+        try{
 
-  const doadores = [
-    {
-      Nome: "Flávio Júlior",
-      CPF: "123.123.123-01",
-    },
-    {
-      Nome: "Rafuel rudin",
-      CPF: "696.969.666-69",
-    },
-    {
-      Nome: "Biel Hesmelinguido",
-      CPF: "123.125.123-01",
-    },
-    {
-      Nome: "Guido Lanches",
-      CPF: "123.123.127-01",
-    },
-    {
-      Nome: "Renata Ingrata",
-      CPF: "152.123.523-01",
-    },
-  ];
+            const response = await axios.get('/doadores/', {
+              withCredentials:true
+            });
+            console.log(JSON.stringify(response?.data))
+            setDoadores(response?.data)
+            
+        }catch(err) {
+            if(!err?.response) {
+                console.log('No server Response')
+            } else if(err.response?.status === 401) {
+                console.log("a")
+            } else {
+                console.log("Login falhou")
+            }
+        }
+    }
+    getContacts()
+  }, [newDoador])
+
+  // const doadores = [
+  //   {
+  //     Nome: "Flávio Júlior",
+  //     CPF: "123.123.123-01",
+  //   },
+  //   {
+  //     Nome: "Rafuel rudin",
+  //     CPF: "696.969.666-69",
+  //   },
+  //   {
+  //     Nome: "Biel Hesmelinguido",
+  //     CPF: "123.125.123-01",
+  //   },
+  //   {
+  //     Nome: "Guido Lanches",
+  //     CPF: "123.123.127-01",
+  //   },
+  //   {
+  //     Nome: "Renata Ingrata",
+  //     CPF: "152.123.523-01",
+  //   },
+  // ];
 
   function handleClickDoador(index) {
     setDoador(index);
@@ -76,7 +102,7 @@ function AdminDoador() {
           infos={doadores}
           indexInfos={doador}
           handleClickInfos={handleClickDoador}
-          showInfos={["Nome", "CPF"]}
+          showInfos={["nome", "cpf"]}
           page={'Doadores'}
         />
       </div>
