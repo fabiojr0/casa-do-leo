c3d793cce9946186f0c417da90f87a0f261fa5a8
Gabriel Hermenegildo <gabriel.f.hermenegildo@gmail.com>
2023-11-02 23:25:13 -0300

    adicionando breakpoint 495px

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,12 @@ function Hero() {
     <div className='relative'>
       <img src='hero-img.png' className='w-screen h-auto brightness-[0.7]' alt='Hero Image'/>
 
-      <div className='absolute flex flex-col justify-center inset-0 gap-1 px-2 lg:p-8 lg:gap-2 text-zinc-100 final-mobile:gap-4'>
-        <h1 className='font-medium text-base sm:text-2xl lg:text-4xl final-mobile:text-lg '>Contribua para a fundação 
+      <div className='absolute flex flex-col justify-center inset-0 gap-1 mobile425:gap-3 px-2 lg:p-8 lg:gap-2 text-zinc-100 final-mobile:gap-4'>
+        <h1 className='font-medium text-base sm:text-2xl lg:text-4xl final-mobile:text-lg final-mobile2:text-xl '>Contribua para a fundação 
           <span className='font-semibold text-green-500 sm:text-2xl lg:text-4xl block'> Casa do Léo</span>
         </h1>
         
-        <p className='text-[0.6rem] w-3/4 sm:text-lg lg:text-2xl lg:w-1/2 final-mobile:text-xs'>Com o seu apoio, podemos fazer mais. Não importa o valor o que vale é a intenção!</p>
+        <p className='text-[0.6rem] w-3/4 mobile425:text-xs sm:text-lg lg:text-2xl lg:w-1/2 final-mobile:text-xs final-mobile2:text-sm'>Com o seu apoio, podemos fazer mais. Não importa o valor o que vale é a intenção!</p>
         <Button>Saiba Mais</Button>
       </div>
     </div>
